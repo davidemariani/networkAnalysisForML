d133a5bf2038a1864d5da7e55f8e4c4485cd2432
davideBbk <dmaria02@dcs.bbk.ac.uk>
2019-01-28 15:31:46 +0000

    node stats utils added

--- features_utils.py
+++ features_utils.py
@@ -9,9 +9,13 @@
 # This file contains several functions to add, process and enhance features        #
 #==================================================================================#
 
+#importing main modules
 import pandas as pd
 import numpy as np
+from scipy import stats
 
+
+#utility functions
 def _xor0(x):
     """
     This function replaces nans with 0
@@ -99,3 +103,59 @@ def add_main_features(inst, ReportDate, impthr=0.009, imp2thr=0.04, purthr=0.009
 
     #discharge amount
     inst[prefix+"has_discharge"] = inst[prefix+"discharge_amount"]>0.001
+
+
+def series_trend(s, applylog=True):
+    """
+    This function defines a trend for a particular given series using linear regression.
+    To be used with invoice_amount for the current dataset, in order to establish the entity of the transactions.
+    """
+    x=np.arange(s.shape[0])
+    slope, intercept, r_value, p_value, std_err = stats.linregress(x,s)
+    #print(slope)
+    if applylog:
+        res = 0 if np.abs(slope)<1e-8 else np.sign(slope) * np.log(np.abs(slope))
+    else:
+        res = slope
+    return res
+
+
+def add_node_stats(inst, igroup, idx, id, ii, prefix, decision_date_col, prefix_read=''):
+    """
+    This function adds stats to each node.
+    inst: instruments dataframe sorted by invoice_date
+    igroup: group of instruments between a certain buyer and a certain seller
+    idx: instrument index in the igroup 
+    id: instrument id
+    ii: instrument features (literally the dataset sliced in correspondence of that instrument)
+    prefix: the prefix to add when creating a new df column
+    prefix_read: the prefix to read when reading a new slice
+    """
+    #adding counter of previously lent in this customer/debtor pair (inst is sorted by invoice date)
+    inst.loc[id, prefix_read+prefix"lent_c"] = idx 
+        
+    #adding counter of previously repaid instruments in this customer/debtor pair
+    #to be repaid, the last payment date needs to be smaller than all the instrument date and the instrument needs to not be open
+    repaid = (igroup.loc[:, prefix_read+"last_payment_date"] < ii[decision_date_col]) & (~ igroup.loc[:, prefix_read+"is_open"]) #filter for repaid instruments in this customer/debtor pair
+    inst.loc[id, prefix_read+prefix+"repaid_c"] = sum(repaid) 
+            
+    #adding counter of previously impaired in this customer/debtor pair
+    inst.loc[id, prefix_read+prefix+"impaired1_c"] = sum(igroup.loc[repaid,prefix_read+"has_impairment1"])
+    inst.loc[id, prefix_read+prefix+"impaired2_c"] = sum(igroup.loc[repaid,prefix_read+"has_impairment2"])
+        
+    #counter of overdue in this customer/debtor pair (considering previous instruments)
+    previous = igroup.index[:idx] #previous instruments selector
+    inst.loc[id, prefix_read+prefix+"pastdue90_c"] = sum((igroup.loc[previous,prefix_read+"due_date"] < ii[decision_date_col] - datetime.timedelta(90)) &
+                                                        igroup.loc[previous,prefix_read+"is_pastdue90"])
+    inst.loc[id, prefix_read+prefix+"pastdue180_c"] = sum((igroup.loc[previous,prefix_read+"due_date"] < ii[decision_date_col] - datetime.timedelta(180)) & 
+                                                          igroup.loc[previous,prefix_read+"is_pastdue180"])
+        
+    #adding trend in amount lent in this customer/debtor pair
+    inst.loc[id, prefix_read+prefix+"trend_a"] = 0 if idx<2 else series_trend(igroup.loc[previous,prefix_read+"invoice_amount"])
+        
+    #adding counter of weekend payments in this pair
+    inst.loc[id, prefix_read+prefix+"we_payment_share"] = igroup.loc[repaid, prefix_read+"we_payment_share"].agg("mean")
+        
+    #adding payment_date_mismatch stats
+    inst.loc[id, prefix_read+prefix+"pd_mismatch_mean"] = igroup.loc[repaid, prefix_read+"payment_date_mismatch"].agg("mean")
+    inst.loc[id, prefix_read+prefix+"pd_mismatch_std"] = igroup.loc[repaid, prefix_read+"payment_date_mismatch"].agg("std") 
