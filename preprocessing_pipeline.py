76cb6c8aa009cbd240539c6230d41d871a20000f
ghizmodave <dmaria02@dcs.bbk.ac.uk>
2019-06-30 23:32:17 +0100

    main functions for preprocessing pipeline created

---
+++ preprocessing_pipeline.py
@@ -0,0 +1,208 @@
+#==================================================================================#
+# Author       : Davide Mariani                                                    #  
+# University   : Birkbeck College, University of London                            # 
+# Programme    : Msc Data Science                                                  #
+# Script Name  : preprocessing_pipeline.py                                         #
+# Description  : utils and scripts for data preprocessing                          #
+# Version      : 0.2                                                               #
+#==================================================================================#
+# This file contains several functions to preprocess data for model feeding        #
+#==================================================================================#
+
+import numpy as np
+import pandas as pd
+import pickle
+import datetime
+from sklearn.pipeline import FeatureUnion, Pipeline
+from sklearn.preprocessing import LabelBinarizer, StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn_pandas import DataFrameMapper, gen_features
+import itertools
+import sys
+
+
+#utils
+class Date2Num(BaseEstimator, TransformerMixin):
+    """
+    Auxiliar class for scikit learn preprocessing module for converting datetimes to float
+    """
+    def __init__(self):
+        return
+    def fit(self, X, y=None):
+        return self
+    def transform(self, X):
+        #print(X[0])
+        nanidx = pd.isnull(X)
+        X1 = np.zeros(X.shape)*np.nan
+        X1[~nanidx] = [float(pd.Timestamp(x).toordinal()) for x in X[~nanidx]]
+        return X1
+
+class ReplaceImputer(BaseEstimator, TransformerMixin):
+    """
+    Auxiliar class for scikit learn preprocessing module for replacing nans
+    """
+    def __init__(self, replacewith=999):
+        self.replacewith = replacewith
+    def fit(self, X, y=None):
+        return self
+    def transform(self, X):
+        X1 = X.copy()
+        X1[np.isnan(X1)] = self.replacewith
+        return X1
+
+
+class LogScaler(BaseEstimator, TransformerMixin):
+    """
+    Auxiliar class for scikit learn preprocessing module for log-scaling quant features
+    """
+    def __init__(self, ZeroNegReplace=1e-5):
+        self.ZeroNegReplace = ZeroNegReplace
+        return
+    def fit(self, X, y=None):
+        return self
+    def transform(self, X):
+        X1=np.float32(X.copy())
+        nanidx = np.isnan(X1)
+        X2 = X1[~nanidx]
+        X2[X2 < self.ZeroNegReplace] = self.ZeroNegReplace
+        #badidx = X2 < 1e-10
+        #print('LogScaler: {:} nans, {:} bads'.format(sum(nanidx), sum(badidx)))
+        #if (sum(badidx)>0):
+        #    print("many bad indices!")
+        #    print(X2[badidx])
+
+        X1[~nanidx] = np.log(X2)
+        return X1
+
+class CapOutliers(BaseEstimator, TransformerMixin):
+    """
+    Auxiliar class for scikit learn preprocessing module for capping outliers greater than M standard dev
+    """
+    def __init__(self, Maxstd=3.):
+        self.Maxstd = Maxstd
+    def fit(self, X, y=None):
+        self.mean = np.nanmean(X)
+        self.std = np.nanstd(X)
+        return self
+    def transform(self, X):
+        X1 = np.float32(X.copy())
+        nanidx = np.isnan(X1)
+        X2 = X1[~nanidx]
+        #print("CapOutliers: {:} nans, {:} mean, {:} std".format(sum(nanidx), self.mean, self.std))
+        bigvals =  (np.abs(X2 - self.mean) > self.Maxstd * self.std)
+        X2[bigvals] = self.mean + self.Maxstd * self.std * np.sign(X2[bigvals] - self.mean)
+        X1[~nanidx] = X2
+        return X1
+
+
+def preprocessing_pipeline(feat_str, feat_quant, feat_exp, feat_date):
+    """
+    This function, given the lists of different type of features, returns a scikit learn preprocessing pipeline
+    ready to be used with the modeule 'transform'
+    """
+    #preparing groups of features for the pipeline
+    feat_str = [[i] for i in feat_str]
+    feat_quant = [[j] for j in feat_quant]
+    feat_exp = [[k] for k in feat_exp]
+    feat_date = [[l] for l in feat_date]
+
+    #pipelines
+    trans_date = gen_features(columns = feat_date,
+                              classes = [{'class': Date2Num},
+                                         {'class': CapOutliers, 'Maxstd': 4},
+                                         {'class': SimpleImputer, 'strategy': "mean"},
+                                         {'class': StandardScaler}])
+
+    trans_quant = gen_features(columns =  feat_quant, 
+                                   classes = [{'class': SimpleImputer, 'strategy': "mean"},
+                                              {'class': CapOutliers, 'Maxstd': 4},
+                                              {'class': StandardScaler}])
+
+    trans_exp = gen_features(columns = feat_exp, 
+                                   classes = [{'class': LogScaler, 'ZeroNegReplace': 1e-3},
+                                              {'class': CapOutliers, 'Maxstd': 4},
+                                              {'class': SimpleImputer, 'strategy': "mean"}, 
+                                              {'class': StandardScaler}])
+
+    trans_str = gen_features(columns = feat_str, 
+                                 classes = [LabelBinarizer])
+
+    preproc_pipeline = DataFrameMapper(trans_quant + trans_exp + trans_str + trans_date)
+
+    return preproc_pipeline
+
+
+def shuffle_train_test(df, trainsize, testsize, testset_control_feature):
+    """
+    This function, given a dataset, train and test size and a time control feature, will return train set and test set
+    based on the initial dataset processed by scikitlearn StratifiedShuffleSplit module.
+    Train set and test set will be created in shuffle mode, without any clear timewise 'cut'.
+    """
+
+    print("Sampling {:} for train and {:} for test sets by shuffling...".format(trainsize, testsize))
+
+    df[testset_control_feature + "_year"] = df[testset_control_feature].apply(lambda x: x.year)
+
+    split = StratifiedShuffleSplit(n_splits=1, 
+                                    train_size = trainsize, 
+                                    test_size = testsize, 
+                                    random_state=42)
+
+    df = df.reset_index(drop=True)
+    
+    #constructing oversampled class y=1 train and test sets:
+    for train_index, test_index in split.split(df, df[testtestset_control_feature]):
+        train_all = df.loc[train_index]
+        test_all = df.loc[test_index]
+
+    return train_all, test_all
+
+
+
+def time_train_test(df, trainsize, testsize, testset_control_feature, testdate):
+    """
+    This function, given a dataset, train and test size and a time control feature, will return train set and test set
+    based on the initial dataset split by a date for which all the instruments before that date will form the train set,
+    while all the instrument past that date will form the test set.
+    """
+
+    print("Splitting train and test sets by time, test cutoff: {:}...".format(testdate))
+
+    test_all  = df.loc[df[testset_control_feature] >= testdate]
+    train_all = df.loc[df[testset_control_feature] <  testdate]
+    print("  {:}({:.1f}%) train, {:}({:.1f}%) test".format(train_all.shape[0], 100*train_all.shape[0]/df.shape[0],
+                                                            test_all.shape[0],   100*test_all.shape[0]/df.shape[0]))
+
+    return train_all, test_all
+
+
+def transform_train_test(train_all, test_all, preproc_pipeline, target_feature):
+    """
+    This function, given the train and test split generated with shuffle_train_test or time_train_test,
+    will use a preproc_pipeline to transform the data returning train and test set ready for the models
+    """
+
+    print("Running the pipeline, target feature is {:}...".format(targetfeature))
+
+    #prepare and save train sets
+    #separate features and labels
+    y_train = train_all[targetfeature].copy().values
+    print("Train y: {:} total, {:} class_1 observations ({:.2f}%) > 0".format(y_train.shape[0], sum(y_train>0), sum(y_train>0)/y_train.shape[0]*100))
+    #apply the pipeline to the training set
+    print("pipeline fit_transform for train set...")
+    X_train = preproc_pipeline.fit_transform(train_all)
+
+    #prepare and save test sets
+    #separate features and labels
+    y_test = test_all[targetfeature].copy().values
+    print("Test y: {:} total, {:} class_1 observations ({:.2f}%) > 0".format(y_test.shape[0], sum(y_test>0), sum(y_test>0)/y_test.shape[0]*100))
+    #apply the pipeline to the training set
+    print("pipeline transform only for test set...")
+    X_test = preproc_pipeline.transform(test_all) 
+
+    #group the category labels together for charting
+    feature_labels = preproc_pipeline.transformed_names_
+
+    return y_train, X_train, y_test, X_test, feature_labels
